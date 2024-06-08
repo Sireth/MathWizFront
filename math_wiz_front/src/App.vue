19cbe50ec72e001a677e4e40b24d2aa5014c5773
Sireth <mltya.usov04@gmail.com>
2024-06-08 09:16:41 +0300

    Project: initial project? added vue

---
+++ math_wiz_front/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+import {ref} from "vue";
+
+const posts = ref([
+  {id: 1, name: "1", body: "1"},
+  {id: 2, name: "2", body: "2"},
+  {id: 3, name: "3", body: "3"},
+])
+
+</script>
+
+<template>
+  <div v-for="item in posts.values()">
+    <div>{{ item.id }}</div>
+    <div>{{ item.name }}</div>
+    <div>{{ item.body }}</div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
